ae2240be09e0820521a1069414cd70f7633f4f65
joakimrrtt <joroa015@osloskolen.no>
2025-01-15 09:10:02 +0100

    d

--- krypteringschat/terminalchat/myapp/urls.py
+++ krypteringschat/terminalchat/myapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
